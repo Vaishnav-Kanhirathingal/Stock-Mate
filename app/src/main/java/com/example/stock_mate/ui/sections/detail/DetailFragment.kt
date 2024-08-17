a988be84e8b25f47e74822e751e06722ee03c681
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2024-08-17 12:04:05 +0530

    2 frags added

---
+++ app/src/main/java/com/example/stock_mate/ui/sections/detail/DetailFragment.kt
@@ -0,0 +1,15 @@
+package com.example.stock_mate.ui.sections.detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+object DetailFragment {
+    object Keys {
+        const val STOCK_COLLECTION_KEY = "STOCK_COLLECTION_KEY"
+    }
+
+    @Composable
+    fun DetailFragment(modifier: Modifier = Modifier) {
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/example/stock_mate/ui/sections/home/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.example.stock_mate.ui.sections.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+object HomeFragment {
+    @Composable
+    fun HomeFragment(modifier: Modifier = Modifier) {
+        TODO()
+    }
+}
